cf38d9c77ea265871751602fd8774aaf93863dc0
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2023-02-07 12:08:12 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#include "Functionality.h"
+#include "charging.h"
 #define twoToPowerTwelve 4094
 #define Max12BitValue 32768
 #define NthBitCalculate(n)(int)(pow(2,n) -1)
